7bbcb1491096376206c1ea84a5d0e4a8b9f45a21
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2020-02-08 16:10:36 -0300

    Fixed the backstack of viewpagers

--- app/src/main/java/br/com/rubensrodrigues/navigationteste/ui/pager/PagerActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/navigationteste/ui/pager/PagerActivity.kt
@@ -1,8 +1,11 @@
 package br.com.rubensrodrigues.navigationteste.ui.pager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.viewpager.widget.ViewPager
 import br.com.rubensrodrigues.navigationteste.R
 
 class PagerActivity : AppCompatActivity() {
@@ -14,7 +17,35 @@ class PagerActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if(!findNavController(R.id.viewpagernavigation).popBackStack())
+        val viewPagerSecondScreen = getViewPager(R.id.viewpager)
+
+        viewPagerSecondScreen?.let {
+            val navController =
+                if (it.currentItem == 0) getPageNavigation(R.id.leftnavigation)
+                else getPageNavigation(R.id.rightnavigation)
+
+            navController?.let { nav -> if (!nav.popBackStack()) popMainNavigation() } ?: run { popMainNavigation() }
+        } ?: kotlin.run { super.onBackPressed() }
+    }
+
+    private fun popMainNavigation() {
+        if (!findNavController(R.id.viewpagernavigation).popBackStack())
             super.onBackPressed()
+    }
+
+    private fun getViewPager(@IdRes id: Int): ViewPager?{
+        return try {
+            findViewById<ViewPager>(id)
+        } catch (ex: IllegalArgumentException){
+            null
+        }
+    }
+
+    private fun getPageNavigation(@IdRes id: Int): NavController?{
+        return try {
+            findNavController(id)
+        } catch (ex: IllegalArgumentException){
+            null
+        }
     }
 }
