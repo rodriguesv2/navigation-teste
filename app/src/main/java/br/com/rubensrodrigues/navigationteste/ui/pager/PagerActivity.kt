9cda0c7cea9a9a3a95bfcdb874590c20350b7962
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2020-02-08 15:04:10 -0300

    Success on the first try to make activity control the stack

--- app/src/main/java/br/com/rubensrodrigues/navigationteste/ui/pager/PagerActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/navigationteste/ui/pager/PagerActivity.kt
@@ -2,6 +2,7 @@ package br.com.rubensrodrigues.navigationteste.ui.pager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import br.com.rubensrodrigues.navigationteste.R
 
 class PagerActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class PagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pager)
+
+    }
+
+    override fun onBackPressed() {
+        if(!findNavController(R.id.viewpagernavigation).popBackStack())
+            super.onBackPressed()
     }
 }
